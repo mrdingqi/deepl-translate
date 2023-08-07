59da7cedc7c547b171ba303b0109051750478b3f
dingqi <68032509@qq.com>
2023-08-07 19:27:59 +0800

    r

--- test_enumerate.py
+++ test_enumerate.py
@@ -6,24 +6,20 @@
 
 
 
-def read_file_in_batches(file_path, batch_size):
+def read_file_in_batches(input_file_path, output_file_path,batch_size=400):
     batch = []
-    with open(file_path, 'r',encoding="utf8") as file:
+    with open(input_file_path, 'r',encoding="utf8") as file:
         for line_number, line in enumerate(file, 1):
             batch.append(line.strip())
             if line_number % batch_size == 0:
-                for i in range(3):
-                    print("*"*50)
+                with open(output_file_path, 'a+',encoding="utf8") as output_file:
+                    output_file.write("\n".join(batch))
                 print(f"Batch {line_number // batch_size}:")
                 print(batch)
                 batch = []
-        # Print the last batch if it's not empty
-        if batch:
 
-            print(f"Batch {line_number // batch_size + 1}:")
-            print(batch)
 
 # 读取文件并每100行为一组存储到数组
-file_path = "D:\\Github\\deepl-translate\\ABOCADETS_content.txt"
-batch_size = 100
-read_file_in_batches(file_path, batch_size)
+input_file_path = "D:\\Github\\deepl-translate\\ABOCADETS_content.txt"
+output_file_path = "D:\\Github\\deepl-translate\\outputfile.txt"
+read_file_in_batches(input_file_path,output_file_path )
